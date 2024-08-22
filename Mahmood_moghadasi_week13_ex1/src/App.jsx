098ac755224ffb2b830435b08a9bd231e5a30306
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-08-23 03:27:10 +0330

    creating tabs content html

--- Mahmood_moghadasi_week13_ex1/src/App.jsx
+++ Mahmood_moghadasi_week13_ex1/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header.jsx";
 import "./App.css";
+
 function App() {
   return (
     <>
--- Mahmood_moghadasi_week13_ex1/src/Header.jsx
+++ Mahmood_moghadasi_week13_ex1/src/Header.jsx
@@ -3,14 +3,22 @@ import "./Header.css";
 function Header() {
   return (
     <>
-      <div>
+      <div className="header-div">
         <h1>Tabs Component with React</h1>
         <nav>
-          <li>Tab1</li>
-          <li>Tab2</li>
-          <li>Tab3</li>
-          <li>Tab4</li>
+          <li>Tab 1</li>
+          <li>Tab 2</li>
+          <li>Tab 3</li>
+          <li>Tab 4</li>
         </nav>
+      </div>
+      <div className="Content">
+        <h2 className="content-title">Content 1</h2>
+        <p className="content-disc">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+          rerum adipisci voluptatibus voluptas fugiat, error saepe illum ea quae
+          officiis?
+        </p>
       </div>
     </>
   );
