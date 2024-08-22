c48a6e8a2734dbae8b3dfe205087a54f009c146e
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-08-22 22:34:49 +0330

    creating header

---
+++ Mahmood_moghadasi_week13_ex1/src/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <div>
+        <h1>Tabs Component with React</h1>
+        <nav>
+          <li>Tab1</li>
+          <li>Tab2</li>
+          <li>Tab3</li>
+          <li>Tab4</li>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Header;
