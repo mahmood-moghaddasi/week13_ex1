8765e44e6b253ff7758a9c3fc9d9d0131f6440ac
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-08-24 20:44:56 +0330

    making tabs dynamic

--- Mahmood_moghadasi_week13_ex1/src/Header.jsx
+++ Mahmood_moghadasi_week13_ex1/src/Header.jsx
@@ -1,19 +1,53 @@
 import "./Header.css";
+import { useState } from "react";
 
 function Header() {
+  const [tabs, setTabs] = useState([
+    { name: "Tab 1", id: 1, isSelect: false },
+    { name: "Tab 2", id: 2, isSelect: false },
+    { name: "Tab 3", id: 3, isSelect: false },
+    { name: "Tab 4", id: 4, isSelect: false },
+  ]);
+
+  // write tabs array with state
+  const [tabNumber, changeNumber] = useState(1);
+
+  const selectHandler = (id) => {
+    console.log(id);
+    const currentTabIndex = tabs.findIndex((tab) => tab.id == id);
+    const newTabs = tabs.map((tab) => {
+      if (tab.id == id) {
+        return { name: tab.name, id: tab.id, isSelect: true };
+      } else {
+        return { name: tab.name, id: tab.id, isSelect: false };
+      }
+    });
+    setTabs(newTabs);
+  };
+  const tabHandler = (event) => {
+    console.dir(event.target);
+    changeNumber(event.target.id);
+    selectHandler(event.target.id);
+  };
   return (
     <>
       <div className="header-div">
         <h1>Tabs Component with React</h1>
         <nav>
-          <li>Tab 1</li>
-          <li>Tab 2</li>
-          <li>Tab 3</li>
-          <li>Tab 4</li>
+          {tabs.map((tab) => (
+            <li
+              className={tab.isSelect ? "showBorder" : ""}
+              id={tab.id}
+              key={tab.id}
+              onClick={tabHandler}
+            >
+              {tab.name}
+            </li>
+          ))}
         </nav>
       </div>
       <div className="Content">
-        <h2 className="content-title">Content 1</h2>
+        <h2 className="content-title">Content {tabNumber}</h2>
         <p className="content-disc">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
           rerum adipisci voluptatibus voluptas fugiat, error saepe illum ea quae
